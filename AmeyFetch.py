bccdd0563c66eb2457961bb76041c88e6c452c6d
Amey Gurjar <techanimationstudio@gmai.com>
2022-07-21 22:15:12 +0530

    AmeyFetch

---
+++ AmeyFetch.py
@@ -0,0 +1,19 @@
+from platform import uname, node
+from getpass import getuser
+from re import S
+from psutil import cpu_count, cpu_freq, cpu_percent, virtual_memory, swap_memory, boot_time, disk_partitions, disk_usage, disk_io_counters
+from GPUtil import getGPUs
+class AmeyFetch:
+	def __init__(self):
+		self.host_name = node()
+		self.user_name = getuser()
+		self.cpu_name = uname().processor
+		self.cpu_count = cpu_count(logical=False)
+		self.cpu_thread = cpu_count(logical=True)
+		self.cpu_freq_max = cpu_freq().max
+		self.ram_usage = virtual_memory().used
+		self.total_ram = virtual_memory().total
+  
+if __name__ == "__main__": 
+	ameyFetch = AmeyFetch()
+	print(AmeyFetch.host_name)
