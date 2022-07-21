dd8a16e812bd594711fc965b1c6f898aa7870206
Amey Gurjar <techanimationstudio@gmai.com>
2022-07-21 22:49:39 +0530

    AmeyFetch

--- AmeyFetch.py
+++ AmeyFetch.py
@@ -15,10 +15,17 @@ class AmeyFetch:
 		self.cpu_freq_max = cpu_freq().max
 		self.ram_usage = virtual_memory().used
 		self.total_ram = virtual_memory().total
-  
+		self.ameyFetchLogo = []
+	def getLogo(self):
+		if len(self.ameyFetchLogo)==0:
+			with open("logo.txt", "r") as logoFile:
+				for line in logoFile:
+					self.ameyFetchLogo.append(line)
+		return self.ameyFetchLogo
 if __name__ == "__main__": 
 	ameyFetch = AmeyFetch()
 	userNameWithHostName = f"{ameyFetch.user_name}@{ameyFetch.host_name}"
 	print("-"*len(userNameWithHostName))
-	print(f"{userNameWithHostName}")
+	for i in range(len(ameyFetch.getLogo())):
+		print(f"{ameyFetch.getLogo()[i]}{userNameWithHostName}".replace("\n", ""))
 	print("-"*len(userNameWithHostName))
